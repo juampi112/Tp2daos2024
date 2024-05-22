8363d94aa8a1b470eb68d5b7433e44bd5875a6bb
Dardo Kuttel <dardo@vertiq.com>
2024-05-22 18:29:10 -0300

    subida inicial del proyecto

---
+++ src/main/java/com/tuti/Daos2024Application.java
@@ -0,0 +1,13 @@
+package com.tuti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daos2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daos2024Application.class, args);
+	}
+
+}
